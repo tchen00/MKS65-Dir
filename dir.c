1d8f1283d72944db7be040edc6658dfb4a327915
Tammy Chen <tchen00@stuy.edu>
2019-11-15 10:47:50 -0500

    started

---
+++ dir.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <dirent.h>
+
+void * readable(float s){
+  int i = 0;
+  char * form[] = {"B", "KB", "MB", "GB"};
+  while (s >= 1024){
+    s /= 1024;
+    i++;
+  }
+  printf("%f%s\n",s,form[i]);
+  return;
+}
+
+int main(){
+
+  return 0;
+
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+all: dir.c
+	gcc dir.c
+run:
+	./a.out
+clean:
+	rm *.out
